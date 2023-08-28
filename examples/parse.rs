ac506c13343b6cf2b239eae9d451de0e2ff0aa00
imlyzh <enterindex@gmail.com>
2023-08-28 09:05:22 +0800

    show integer literal parse bug

--- examples/parse.rs
+++ examples/parse.rs
@@ -3,11 +3,7 @@ use std::fs;
 fn main() {
   print!(">>> ");
   // let test = fs::read_to_string("./test.txt").unwrap();
-  let test = "struct user {
-    name: string = 1,
-    age: uint = 2,
-    desc: string = 3
-  }";
-  let r = struct_def(&test);
+  let test = "recv 1 -> recv string -> send 114 -> send int -> end";
+  let r = _type(&test);
   println!("> {:?}", r);
 }
