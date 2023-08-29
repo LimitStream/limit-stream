3fa7b73741326888bb60b8a7d6d0e14af9b271f4
LemonHX <lemonhx@lemonhx.moe>
2023-08-30 00:11:24 +0200

    rename module

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,3 @@
 pub mod ast;
 pub mod parser;
-pub mod graph;
+pub mod type_checker;
---
+++ src/type_checker.rs
@@ -0,0 +1,8 @@
+//! Type checker
+//! 
+//! steps:
+//! 1. check `end` exists?
+//! 2. check `ring` exists?
+//! 3. check if could generate dual type
+//! 
+//! 
